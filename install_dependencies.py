cb9ad15ce4f47660c66985b209ed80a529a835eb
RainingComputers <vishnu.vish.shankar@gmail.com>
2018-05-22 18:30:03 +0530

    Added win32api to install_dependencies.py script

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,4 +1,14 @@
 import pip
+import platform
+
+print('Installing dependencies for whipftp...')
+print('Platform:', platform.system())
 
 pip.main(['install', 'paramiko'])
+
 pip.main(['install', 'psutil'])
+
+if(platform.system() == 'Windows'):
+    pip.main(['install', 'win32api'])
+else:
+    print('No need to install win32api.')
