24955455980fa3bafc6fae58c98ad81bd9e7634c
RainingComputers <vishnu.vish.shankar@gmail.com>
2018-08-08 09:16:03 +0530

    Updated install_dependencies.py script

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 
+#Dependencies to be installed:
+#   pramiko
+#   psutil
+#   pypiwin32
+
+
+import os
+import platform
+import sys
+
 try:
     import pip
-    import platform
-except:
-    print('/!\ Failed to install dependencies, is pip installed?')
-    input('Press Enter to continue...')
+except ImportError:
+    print('Please install pip. Failed to install dependencies.')
+    input('Press enter to continue...')
     exit()
 
-print('Installing dependencies for whipftp...')
-print('Platform:', platform.system())
-
-pip.main(['install', 'paramiko'])
-
-pip.main(['install', 'psutil'])
-
 if(platform.system() == 'Windows'):
-    pip.main(['install', 'pypiwin32'])
-else:
-    print('No need to install pypiwin32.')
+    print('Platform: Windows')
+    python_path = sys.executable
+    if(python_path.split('\\')[-1:][0] == 'pythonw.exe'):
+        print('Do not run this from IDLE. Double click the script to run it directly from python.exe')
+        input('Press enter to continue...')
+        exit()
+    os.system(python_path + ' -m pip install --upgrade pip')
+    os.system(python_path + ' -m pip install paramiko')
+    os.system(python_path + ' -m pip install psutil')
+    os.system(python_path + ' -m pip install pypiwin32')
+    input('Press enter to continue...')
 
-input('Press Enter to continue...')
+if(platform.system() == 'Linux'):
+    print('Platform: Linux')
+    os.system('python3 -m pip install --upgrade pip')
+    os.system('python3 -m pip install paramiko')
+    os.system('python3 -m pip install psutil')
+    input('Press enter to continue...')
