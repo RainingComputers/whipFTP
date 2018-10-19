1209036069ea0b34da84b8c9e6594828af209342
RainingComputers <vishnu.vish.shankar@gmail.com>
2018-10-19 18:31:01 +0530

    Fixed mount pint lists, /boot/efi is not included

--- linux_mount.py
+++ linux_mount.py
@@ -5,9 +5,10 @@ def get_mounts():
     f = open('/proc/mounts')
     for line in f:
         details = line.split()
-        if('/dev/sdb' in details[0] or ('/dev/sda' in details[0] and '/boot' not in details[1])):
-            details_decoded_string = bytes(details[1], "utf-8").decode("unicode_escape")
-            mountpoints.append(details_decoded_string)
+        if('/dev/sdb' in details[0] or '/dev/sda' in details[0]):
+            if(details[1] != '/boot/efi'):
+                details_decoded_string = bytes(details[1], "utf-8").decode("unicode_escape")
+                mountpoints.append(details_decoded_string)
 
     f.close()
     return mountpoints
